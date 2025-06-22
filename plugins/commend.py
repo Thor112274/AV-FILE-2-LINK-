b588b69be5ab4be24bf8cac5343aed7f887e1b92
jonysins2211 <Jonysins11220@gmail.com>
2025-06-23 00:24:10 +0530

    Update commend.py

--- plugins/commend.py
+++ plugins/commend.py
@@ -127,7 +127,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         if len(str(user_id)) == 10:
             reason = str(query.data.split("_")[2])
             try:
-                await client.send_message(user_id , f"<b>ʏᴏᴜ ᴀʀᴇ ʙᴀɴɴᴇᴅ ʙʏ [ᴀᴠ ᴄʜᴀᴛ ᴏᴡɴᴇʀ](https://telegram.me/Mladminbot)\nʀᴇᴀsᴏɴ : {reason}</b>")
+                await client.send_message(user_id , f"<b>ʏᴏᴜ ᴀʀᴇ ʙᴀɴɴᴇᴅ ʙʏ [Bot ᴏᴡɴᴇʀ](https://telegram.me/Mladminbot)\nʀᴇᴀsᴏɴ : {reason}</b>")
                 await query.message.edit(f"<b>Aʟᴇʀᴛ sᴇɴᴛ ᴛᴏ <code>{user_id}</code>\nʀᴇᴀsᴏɴ : {reason}</b>")
             except Exception as e:
                 await query.message.edit(f"<b>sʀʏ ɪ ɢᴏᴛ ᴛʜɪs ᴇʀʀᴏʀ : {e}</b>")
