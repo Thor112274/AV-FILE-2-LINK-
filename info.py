7b15a00c75ba0e483b41567e7d78027cce8cf1d8
Thor112274 <126616021+Thor112274@users.noreply.github.com>
2025-07-09 19:45:17 +0530

    Update info.py

--- info.py
+++ info.py
@@ -9,23 +9,23 @@ id_pattern = re.compile(r'^.\d+$')
 
 # Bot information
 SESSION = environ.get('SESSION', 'Webavbot')
-API_ID = int(environ.get('API_ID', '27258953'))
-API_HASH = environ.get('API_HASH', '0add43fc460daca0a86077989cfc414f')
+API_ID = int(environ.get('API_ID', '10685201'))
+API_HASH = environ.get('API_HASH', '8e039b83a886a2c2b97309ccc6298c20')
 BOT_TOKEN = environ.get('BOT_TOKEN', "")
-BOT_USERNAME = environ.get("BOT_USERNAME", 'mlfiletolinkbot') # without @ 
+BOT_USERNAME = environ.get("BOT_USERNAME", 'Telex_File2link_Bot') # without @ 
 
 # Admins, Channels & Users
-BIN_CHANNEL = int(environ.get("BIN_CHANNEL", '-1002858307046')) # admin your channel in stream 
-LOG_CHANNEL = int(environ.get("LOG_CHANNEL", '-1002858307046')) # admin your channel in users log 
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '949657126').split()] # 3567788, 678899, 5889467
-OWNER_USERNAME = environ.get("OWNER_USERNAME", 'mladminbot') # without @ 
+BIN_CHANNEL = int(environ.get("BIN_CHANNEL", '-1001948256614')) # admin your channel in stream 
+LOG_CHANNEL = int(environ.get("LOG_CHANNEL", '-1001948256614')) # admin your channel in users log 
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '982105601').split()] # 3567788, 678899, 5889467
+OWNER_USERNAME = environ.get("OWNER_USERNAME", 'Spidey_Professor') # without @ 
 
 # pics information
-PICS = environ.get('PICS', 'https://graph.org/file/a319f6b9ce3b993c6e22f.jpg')
+PICS = environ.get('PICS', 'https://i.ibb.co/FbGdLJN5/photo-2025-07-09-13-56-46-7525080846336786448.jpg')
 
 # channel link information
-CHANNEL = environ.get('CHANNEL', 'https://t.me/MovieEntertainment4u')
-SUPPORT = environ.get('SUPPORT', 'https://t.me/Movie_loverzz')
+CHANNEL = environ.get('CHANNEL', 'https://t.me/TelexOriginals')
+SUPPORT = environ.get('SUPPORT', 'https://t.me/TelexOriginals')
 
 #Dont Remove My Credit @AV_BOTz_UPDATE 
 #This Repo Is By @BOT_OWNER26 
@@ -51,16 +51,16 @@ BAN_CHNL = [int(ban_chal) if id_pattern.search(ban_chal) else ban_chal for ban_c
 BAN_ALERT = environ.get('BAN_ALERT' , '<b>ʏᴏᴜʀ ᴀʀᴇ ʙᴀɴɴᴇᴅ ᴛᴏ ᴜsᴇ ᴛʜɪs ʙᴏᴛ.ᴄᴏɴᴛᴀᴄᴛ [ ᴄʜᴀᴛ ᴏᴡɴᴇʀ](https://telegram.me/Mladminbot) ᴛᴏ ʀᴇsᴏʟᴠᴇ ᴛʜᴇ ɪssᴜᴇ!!</b>')
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://primebaby11220:TuGa0I5ZsiC8mp5A@cluster0.df4rivj.mongodb.net/?retryWrites=true&w=majority")
-DATABASE_NAME = environ.get('DATABASE_NAME', "primebaby11220")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://tgarun163:qgq7VlZ1a4Ke9Dnh@cluster0.7iaqu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DATABASE_NAME = environ.get('DATABASE_NAME', "cluster0")
 
 # fsub  information
-AUTH_PICS = environ.get('AUTH_PICS', 'https://graph.org/file/a319f6b9ce3b993c6e22f.jpg')              
-AUTH_CHANNEL = (environ.get("AUTH_CHANNEL", "-1002230197603"))
+AUTH_PICS = environ.get('AUTH_PICS', 'https://i.ibb.co/FbGdLJN5/photo-2025-07-09-13-56-46-7525080846336786448.jpg')              
+AUTH_CHANNEL = (environ.get("AUTH_CHANNEL", "-1001640099591"))
 FSUB = environ.get("FSUB", True)
 
 # port information
-PORT = int(getenv('PORT', '2626'))
+PORT = int(getenv('PORT', '8080'))
 NO_PORT = bool(getenv('NO_PORT', False))
 
 #Dont Remove My Credit @AV_BOTz_UPDATE 
